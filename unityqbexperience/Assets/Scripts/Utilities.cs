74914da76ae43ebd7eacb97f2c04ad2bef371350
Lukas Kuhn <lukas.m.kuhn@gmail.com>
2019-05-28 12:42:53 +0200

    Utilities hinzugefügt, welche zwei Funktionen hält um Yards in Meter und Meter in Yards umzurechnen.

---
+++ unityqbexperience/Assets/Scripts/Utilities.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Utilities {
+
+private static double yardCoefficient = 1.0936;
+    public static double meterInYards(double meter) {
+        return (meter * yardCoefficient);
+    }
+
+    public static double yardsInMeter(double yards) {
+        return (yards / yardCoefficient);
+    }
+}
